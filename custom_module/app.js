a7018869ab141076e5bc12992fdd87293c8a03fd
adityaprakash0 <aditya.prakash_cs23@gla.ac.in>
2026-01-22 21:46:47 +0530

    [Aditya Prakash].add file custom string utility module

---
+++ custom_module/app.js
@@ -0,0 +1,7 @@
+const stringUtils = require('./stringUtils');
+
+const text = 'hello world';
+
+console.log(stringUtils.capitalize(text));
+console.log(stringUtils.reverseString(text));
+console.log(stringUtils.countVowels(text));
